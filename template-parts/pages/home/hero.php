02a5c961c844e646e90b5ec5b3194300acc858fb
Daniel-Digid <daniel@dig.id>
2024-04-17 15:47:52 +0100

    d

--- template-parts/pages/home/hero.php
+++ template-parts/pages/home/hero.php
@@ -1,3 +1,12 @@
-<section class="section-hero">
-	test
+<section class="section-hero bg-dark-blue">
+	<div class="st-grid relative items-center">
+		<div class="col-span-2 md:col-span-4 xl:col-span-4 col-start-1 md:col-start-2 xl:col-start-2 z-10 absolute" style="user-select: none;">
+			<p class="st-subtitle-bi-caps text-light-blue"><?php esc_html_e( 'welcome to the official site of', 'stricker' ) ?></p>
+			<p class="st-title-big st-text-outline-big text-transparent"><?php esc_html_e( 'DOMINIC', 'stricker' ) ?></p>
+			<p class="st-title-big st-text-outline-big text-blue"><?php esc_html_e( 'STRICKER', 'stricker' ) ?></p>
+		</div>
+		<div class="hidden xl:block xl:col-span-9 xl:col-start-4 bg-black h-[824px] top-0 right-0 w-full z-0">
+			test
+		</div>
+	</div>
 </section>
