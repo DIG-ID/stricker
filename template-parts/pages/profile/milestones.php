9e80fb0c66d073485b9ebaf662e664131fe4f1e6
Daniel-Digid <daniel@dig.id>
2024-05-21 13:21:31 +0100

    d

--- template-parts/pages/profile/milestones.php
+++ template-parts/pages/profile/milestones.php
@@ -1,7 +1,7 @@
 <section class="section-milestones relative bg-[#DFF4F7]">
     <div class="scroll-section relative h-[101vh]">
-        <div class="panel w-[100vw] md:w-[130vw] xl:w-[100vw] st-grid !px-0">
-            <div class="col-span-1 md:col-span-4 xl:col-span-7 grid grid-cols-2 md:grid-cols-6 xl:grid-cols-7 gap-6 xl:gap-8 px-6 md:px-0 bg-dark-blue">
+        <div class="panel w-[150vw] md:w-[150vw] xl:w-[100vw] st-grid !px-0">
+            <div class="col-span-1 md:col-span-3 xl:col-span-7 grid grid-cols-2 md:grid-cols-6 xl:grid-cols-7 gap-6 xl:gap-8 px-6 md:px-0 bg-dark-blue">
                 <div class="col-span-2 md:col-span-4 xl:col-span-3 col-start-1 md:col-start-2 xl:col-start-2 flex flex-col justify-center py-28">
                     <p class="st-title text-blue mb-5"><?php the_field( 'milestones_intro_title' ); ?></p>
                     <p class="st-subtitle-i text-light-blue text-wrap mb-28"><?php the_field( 'milestones_intro_subtitle' ); ?></p>
@@ -12,8 +12,8 @@
                 </div>
             </div>
 
-            <div class="col-span-1 md:col-span-2 xl:col-span-5 col-start-2 md:col-start-5 xl:col-start-8 grid grid-cols-2 md:grid-cols-6 xl:grid-cols-5 gap-6 xl:gap-8 px-6 md:px-0 bg-[#DFF4F7] py-28">
-                <div class="col-span-2 md:col-span-3 xl:col-span-3 z-10">
+            <div class="col-span-1 md:col-span-3 xl:col-span-5 col-start-2 md:col-start-4 xl:col-start-8 grid grid-cols-2 md:grid-cols-6 xl:grid-cols-5 gap-6 xl:gap-8 px-6 md:px-0 bg-[#DFF4F7] py-28">
+                <div class="col-span-2 md:col-span-5 xl:col-span-3 z-10">
                 <?php 
                 $image_intro = get_field( 'milestones_content_image_1' );
                 if ( $image_intro ) : ?>
@@ -21,14 +21,14 @@
                 <?php endif;
                 ?>
                 </div>
-                <div class="col-span-2 md:col-span-4 xl:col-span-3 col-start-1 md:col-start-2 xl:col-start-2 xl:col-end-5 flex flex-col justify-end items-end">
+                <div class="col-span-2 md:col-span-4 xl:col-span-5 col-start-1 md:col-start-2 xl:col-start-2 xl:col-end-5 flex flex-col justify-end items-end">
                     <p class="st-subtitle-i w-fit"><?php the_field( 'milestones_content_text_1' ); ?></p>
                 </div>
             </div>
         </div>
 
-        <div class="panel st-grid w-[100vw] md:w-[130vw] xl:w-[100vw] py-28 bg-[#DFF4F7]">
-            <div class="col-span-1 md:col-span-4 xl:col-span-4">
+        <div class="panel st-grid w-[150vw] md:w-[150vw] xl:w-[100vw] py-28 bg-[#DFF4F7]">
+            <div class="col-span-1 md:col-span-3 xl:col-span-4 flex flex-col justify-center xl:block">
                 <p class="st-subtitle-i mb-24"><?php the_field( 'milestones_content_text_2' ); ?></p>
                 <?php 
                 $image = get_field( 'milestones_content_image_2' );
@@ -37,34 +37,34 @@
                 <?php endif;
                 ?>
             </div>
-            <div class="col-span-1 md:col-span-2 xl:col-span-5 col-start-2 md:col-start-5 xl:col-start-7 relative">
+            <div class="col-span-1 md:col-span-3 xl:col-span-5 col-start-2 md:col-start-4 xl:col-start-7 relative px-12 xl:px-0">
                 <?php 
                 $image = get_field( 'milestones_content_image_3' );
                 if ( $image ) : ?>
                     <?php echo wp_get_attachment_image( $image, 'full', false, array( 'class' => 'w-full object-cover' ) ); ?>
                 <?php endif;
                 ?>
-                <p class="st-subtitle-i mb-24 xl:mt-20 xl:ml-20 max-w-[374px]"><?php the_field( 'milestones_content_text_3' ); ?></p>
+                <p class="st-subtitle-i mb-24 mt-10 xl:mt-20 xl:ml-20 max-w-[374px]"><?php the_field( 'milestones_content_text_3' ); ?></p>
                 <?php 
                 $image = get_field( 'milestones_content_image_3-2' );
                 if ( $image ) : ?>
-                    <?php echo wp_get_attachment_image( $image, 'full', false, array( 'class' => 'w-full object-cover absolute max-w-[334px] top-[30%] -right-[20%]' ) ); ?>
+                    <?php echo wp_get_attachment_image( $image, 'full', false, array( 'class' => 'w-full object-cover absolute max-w-[334px] top-[30%] -right-[20%] hidden xl:block' ) ); ?>
                 <?php endif;
                 ?>
             </div>
         </div>
 
-        <div class="panel st-grid w-[100vw] md:w-[130vw] xl:w-[100vw] py-28 bg-[#DFF4F7]">
-            <div class="col-span-1 md:col-span-4 xl:col-span-4 col-start-1 md:col-start-1 xl:col-start-2">
+        <div class="panel st-grid w-[150vw] md:w-[150vw] xl:w-[100vw] py-28 bg-[#DFF4F7]">
+            <div class="col-span-1 md:col-span-3 xl:col-span-4 col-start-1 md:col-start-1 xl:col-start-2 flex flex-col justify-end xl:block">
                 <p class="st-subtitle-i mb-24 max-w-[374px]"><?php the_field( 'milestones_content_text_4' ); ?></p>
                 <?php 
                 $image = get_field( 'milestones_content_image_4' );
                 if ( $image ) : ?>
-                    <?php echo wp_get_attachment_image( $image, 'full', false, array( 'class' => 'w-full object-cover ml-20' ) ); ?>
+                    <?php echo wp_get_attachment_image( $image, 'full', false, array( 'class' => 'w-full object-cover xl:ml-20' ) ); ?>
                 <?php endif;
                 ?>
             </div>
-            <div class="col-span-1 md:col-span-2 xl:col-span-4 col-start-2 md:col-start-5 xl:col-start-8">
+            <div class="col-span-1 md:col-span-3 xl:col-span-4 col-start-2 md:col-start-4 xl:col-start-8">
                 <?php 
                 $image = get_field( 'milestones_content_image_5' );
                 if ( $image ) : ?>
@@ -75,21 +75,21 @@
             </div>
         </div>
 
-        <div class="panel st-grid w-[100vw] md:w-[130vw] xl:w-[100vw] py-28 bg-[#DFF4F7]">
-            <div class="col-span-1 md:col-span-4 xl:col-span-4 grid grid-cols-1 md:grid-cols-4 xl:grid-cols-3 col-start-1 md:col-start-1 xl:col-start-2">
-                <div class="col-span-1 md:col-span-4 xl:col-span-2 col-start-1 md:col-start-1 xl:col-start-2">
+        <div class="panel st-grid w-[150vw] md:w-[150vw] xl:w-[100vw] py-28 bg-[#DFF4F7]">
+            <div class="col-span-1 md:col-span-3 xl:col-span-4 grid grid-cols-1 md:grid-cols-4 xl:grid-cols-3 col-start-1 md:col-start-1 xl:col-start-2 px-12 xl:px-0">
+                <div class="col-span-1 md:col-span-3 xl:col-span-2 col-start-1 md:col-start-1 xl:col-start-2 flex flex-col justify-end items-start xl:block">
                     <p class="st-subtitle-i mb-24"><?php the_field( 'milestones_content_text_6' ); ?></p>
                 </div>
-                <div class="col-span-1 md:col-span-4 xl:col-span-2 col-start-1">
+                <div class="col-span-1 md:col-span-4 xl:col-span-2 col-start-1 md:col-start-2 xl:col-start-1 flex flex-col items-center justify-end xl:block">
                     <?php 
                     $image = get_field( 'milestones_content_image_6-2' );
                     if ( $image ) : ?>
-                        <?php echo wp_get_attachment_image( $image, 'full', false, array( 'class' => 'w-full object-cover max-w-[334px]' ) ); ?>
+                        <?php echo wp_get_attachment_image( $image, 'full', false, array( 'class' => 'w-full object-cover max-w-[450px] xl:max-w-[334px]' ) ); ?>
                     <?php endif;
                     ?>
                 </div>
             </div>
-            <div class="col-span-1 md:col-span-2 xl:col-span-5 col-start-2 md:col-start-5 xl:col-start-7">
+            <div class="col-span-1 md:col-span-3 xl:col-span-5 col-start-2 md:col-start-4 xl:col-start-7 flex flex-col justify-start xl:block">
                 <?php 
                 $image = get_field( 'milestones_content_image_6' );
                 if ( $image ) : ?>
@@ -99,9 +99,9 @@
             </div>
         </div>
 
-        <div class="panel st-grid w-[100vw] md:w-[130vw] xl:w-[100vw] py-28 bg-[#DFF4F7]">
-            <div class="col-span-1 md:col-span-4 xl:col-span-5 grid grid-cols-1 md:grid-cols-4 xl:grid-cols-5 col-start-1 md:col-start-1 xl:col-start-2">
-                <div class="col-span-1 md:col-span-4 xl:col-span-3 col-start-1 md:col-start-1 xl:col-start-2">
+        <div class="panel st-grid w-[150vw] md:w-[147vw] xl:w-[100vw] py-28 bg-[#DFF4F7]">
+            <div class="col-span-1 md:col-span-3 xl:col-span-5 grid grid-cols-1 md:grid-cols-4 xl:grid-cols-5 col-start-1 md:col-start-1 xl:col-start-2 px-12 xl:px-0">
+                <div class="col-span-1 md:col-span-4 xl:col-span-3 col-start-1 md:col-start-1 xl:col-start-2 flex flex-col justify-end xl:block">
                     <p class="st-subtitle-i mb-24"><?php the_field( 'milestones_content_text_7' ); ?></p>
                 </div>
                 <div class="col-span-1 md:col-span-4 xl:col-span-5 col-start-1">
@@ -113,7 +113,7 @@
                     ?>
                 </div>
             </div>
-            <div class="col-span-1 md:col-span-2 xl:col-span-4 col-start-2 md:col-start-5 xl:col-start-7 xl:-ml-32">
+            <div class="col-span-1 md:col-span-3 xl:col-span-4 col-start-2 md:col-start-4 xl:col-start-7 xl:-ml-32 hidden xl:block">
                 <?php 
                 $image = get_field( 'milestones_content_image_7-2' );
                 if ( $image ) : ?>
@@ -123,11 +123,11 @@
             </div>
         </div>
 
-        <div class="panel st-grid w-[100vw] md:w-[150vw] xl:w-[100vw] py-28 bg-[#DFF4F7]">
-            <div class="col-span-1 md:col-span-4 xl:col-span-3 flex items-center">
+        <div class="panel st-grid w-[150vw] md:w-[163vw] xl:w-[100vw] py-28 bg-[#DFF4F7]">
+            <div class="col-span-1 md:col-span-2 xl:col-span-3 flex items-center">
                 <p class="st-subtitle-i mb-24"><?php the_field( 'milestones_content_text_8' ); ?></p>
             </div>
-            <div class="col-span-1 md:col-span-2 xl:col-span-4 xl:max-w-[454px] col-start-2 md:col-start-5 xl:col-start-4">
+            <div class="col-span-1 md:col-span-2 xl:col-span-4 xl:max-w-[454px] col-start-2 md:col-start-3 xl:col-start-4 flex flex-col justify-center xl:block">
                 <?php 
                 $image = get_field( 'milestones_content_image_8' );
                 if ( $image ) : ?>
