5db56f980093e4c2202215f4bed8f398448c622d
Daniel-Digid <daniel@dig.id>
2024-06-27 12:13:10 +0100

    d

--- template-parts/pages/page-content.php
+++ template-parts/pages/page-content.php
@@ -1,10 +1,18 @@
-<section class="section-data-content pt-28 pb-16">
-	<div class="hs-container ">
-		<div class="hs-grid">
-			<div class="col-span-1 md:col-span-8">
-				<h1 class="title__page"><?php the_title(); ?></h1>
-				<div class="text__description text__data-content"><?php the_content(); ?></div>
-			</div>
-		</div>
+
+<div class="page-default pt-28 xl:pt-48 pb-16 md:pb-32 xl:pb-60 overflow-hidden">
+	<div class="st-grid">
+        <div class="st-container st-container-grid">
+            <div class="col-span-2 md:col-span-6 xl:col-span-10"><?php do_action( 'breadcrumbs' ); ?></div>
+            
+            <div class="col-span-2 md:col-span-6 xl:col-span-7 grid grid-cols-2 md:grid-cols-6 xl:grid-cols-7">
+                <div class="col-span-2 md:col-span-6 xl:col-span-7 mb-7 xl:mb-0">
+					<div class="st-subtitle-bi text-dark-blue my-5"><?php the_title(); ?></div>
+                    <div class="st-body-dates text-dark-blue mb-20"><?php the_content(); ?></div>
+                </div>
+            </div>
+            <div class="col-span-2 md:col-span-6 xl:col-span-3">
+                
+            </div>
+        </div>
 	</div>
-</section>
+</div>
