7b4bd8457b53761c532bd6d1c7f3401123210b05
Daniel-Digid <daniel@dig.id>
2024-06-27 16:17:33 +0100

    d

--- template-parts/pages/home/profile.php
+++ template-parts/pages/home/profile.php
@@ -25,7 +25,7 @@
         </div>
         <div class="col-span-1 md:col-span-3 xl:col-span-12 xl:col-start-5 xl:col-end-13 xl:grid xl:grid-cols-8 gap-6 xl:gap-8 row-span-2 md:pt-9">
             <div class="xl:w-80 flex mb-[40px] md:mb-[75px] xl:mb-0">
-                <div class="st-title-big !not-italic text-blue w-[80px] md:w-[190px]"><span class="number-anim inline-block w-full border-r-2 border-light-blue md:pb-7 md:leading-[130px]"><?php the_field( 'profile_singles_ranking' ); ?></span></div>
+                <div class="st-title-big !not-italic text-blue w-[100px] md:w-[260px]"><span class="number-anim inline-block w-full border-r-2 border-light-blue md:pb-7 md:leading-[130px]"><?php the_field( 'profile_singles_ranking' ); ?></span></div>
                 <div class="st-subtitle-bi-caps text-light-blue w-[60px] md:w-[130px] flex justify-end pt-2">Singles<br>Ranking</div>
             </div>
             <div class="xl:w-[260px] xl:col-start-5 flex">
