bdcdf9728893eabd6b849b2efa398f15706ffafe
Daniel-Digid <daniel@dig.id>
2024-08-22 11:53:08 +0100

    d

--- template-parts/components/card-news-preview.php
+++ template-parts/components/card-news-preview.php
@@ -1,23 +1,23 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class( 'card-news mb-10 bg-light-blue border-t-[7px] border-blue' ); ?>>
-<a href="<?php the_permalink(); ?>" class="grid grid-cols-4 md:grid-cols-6 xl:grid-cols-3">
-    <div class="col-span-1 md:col-span-2 xl:col-span-1 col-start-1 md:col-start-1 xl:col-start-1">
-        <?php if (has_post_thumbnail()) : 
-            $full_image_url = wp_get_attachment_image_src(get_post_thumbnail_id(), 'overall-square');
-        ?>
-            <div class="featured-image">
-                <img src="<?php echo $full_image_url[0]; ?>" alt="<?php the_title(); ?>" class="object-cover w-full" />
-            </div>
-        <?php else : ?>
-            <div class="featured-image">
-                <img src="https://placehold.co/453x453" class="object-cover w-full size-overall-square">
-            </div>
-        <?php endif; ?>
-    </div>
-    <div class="col-span-3 md:col-span-4 xl:col-span-2 col-start-2 md:col-start-3 xl:col-start-2 py-4">
-        <?php $formatted_date = get_the_date('d/m/y');
-        echo '<p class="st-body-dates text-blue px-5">' . $formatted_date . '</p>';
-        ?>
-        <h2 class="st-subtitle-i !text-[17px] !font-[545] text-dark-blue px-5"><?php the_title(); ?></h2>
-    </div>
-</a>
+    <a href="<?php the_permalink(); ?>" class="grid grid-cols-4 md:grid-cols-6 xl:grid-cols-3">
+        <div class="col-span-1 md:col-span-2 xl:col-span-1 col-start-1 md:col-start-1 xl:col-start-1">
+            <?php if (has_post_thumbnail()) : 
+                $full_image_url = wp_get_attachment_image_src(get_post_thumbnail_id(), 'overall-square');
+            ?>
+                <div class="featured-image h-full">
+                    <img src="<?php echo $full_image_url[0]; ?>" alt="<?php the_title(); ?>" class="object-cover w-full h-full" />
+                </div>
+            <?php else : ?>
+                <div class="featured-image">
+                    <img src="https://placehold.co/453x453" class="object-cover w-full size-overall-square">
+                </div>
+            <?php endif; ?>
+        </div>
+        <div class="col-span-3 md:col-span-4 xl:col-span-2 col-start-2 md:col-start-3 xl:col-start-2 py-4 min-h-[86px] md:min-h-[112px]">
+            <?php $formatted_date = get_the_date('d/m/y');
+            echo '<p class="st-body-dates text-blue px-5">' . $formatted_date . '</p>';
+            ?>
+            <h2 class="st-subtitle-i !leading-normal !text-[14px] md:!text-[17px] !font-[545] text-dark-blue px-5"><?php the_title(); ?></h2>
+        </div>
+    </a>
 </article>
