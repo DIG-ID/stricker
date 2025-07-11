df7a315ce2f901bf094fe285771b13cb57c96fe2
Daniel-Digid <daniel@dig.id>
2025-07-11 16:53:59 +0100

    up

--- page-templates/page-contacts.php
+++ page-templates/page-contacts.php
@@ -10,7 +10,7 @@ if ( have_posts() ) :
 		do_action( 'before_main_content' );
 			get_template_part( 'template-parts/pages/contact/header' );
 			get_template_part( 'template-parts/pages/contact/content' );
-			get_template_part( 'template-parts/pages/contact/support' );	
+			//get_template_part( 'template-parts/pages/contact/support' );
 		do_action( 'after_main_content' );
 	endwhile;
 endif;
