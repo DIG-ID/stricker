80c3feae1e5e34d00dbdf5b6461173d19dae81bf
bankz91 <brunoduarte.91300@gmail.com>
2026-04-21 16:20:25 +0100

    1

--- header.php
+++ header.php
@@ -14,5 +14,5 @@
 		<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TB4JNL4"height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 		<!-- End Google Tag Manager (noscript) -->
 		<?php do_action( 'wp_body_open' ); ?>
-		<a class="skip-link" href="#main-content"><?php esc_html_e( 'Skip to content', 'stricker' ); ?></a>
+		<!-- <a class="skip-link" href="#main-content"><?php esc_html_e( 'Skip to content', 'stricker' ); ?></a> -->
 		<?php get_template_part( 'template-parts/header', 'main' ); ?>
